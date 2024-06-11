4924384e5f4f2fa6def6272c94c2b214a030b071
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-06-11 10:28:28 +0200

    add global var css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-pink-600">Hello world!</h1>
-  );
+  return <h1 className="text-[var(--pink-color)] md:text-5xl">Hello world!</h1>;
 }
