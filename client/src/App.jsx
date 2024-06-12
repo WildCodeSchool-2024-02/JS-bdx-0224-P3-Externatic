eab9d9960471ccca29d1a3f7782cd663253876c0
JulienChevreauPro <julienchevreau33@gmail.com>
2024-06-12 17:22:23 +0200

    global react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
-      <NavBar />
       <Outlet />
       <ScrollToTop />
       <Footer />
