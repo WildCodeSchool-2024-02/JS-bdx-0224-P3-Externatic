806ac44a65dae1d958ae1c9bd4820838a99a8071
Enzo Michel <enzomichel399@gmail.com>
2024-06-13 10:36:44 +0200

    some avancement

--- client/src/components/atomic/buttons/ButtonOffer.jsx
+++ client/src/components/atomic/buttons/ButtonOffer.jsx
@@ -1,12 +1,12 @@
 function ButtonOffer() {
   return (
-  <button
-  className=" w-56 h-11 bg-[var(--primary-color)] font-semibold text-[var(--primary-background-color)] rounded-lg cursor-pointer max-md:w-36 max-md:h-8 max-md:text-sm"
-  type="button"
->
-  VOIR L'OFFRE
-</button>
-  )
+    <button
+      className=" px-5 py-2 bg-[var(--primary-color)] font-semibold text-[var(--primary-background-color)] rounded-lg cursor-pointer max-md:w-36 max-md:h-8 max-md:text-sm"
+      type="button"
+    >
+      VOIR L'OFFRE
+    </button>
+  );
 }
 
 export default ButtonOffer;
--- client/src/components/atomic/card/Card.jsx
+++ client/src/components/atomic/card/Card.jsx
@@ -1,41 +1,28 @@
+import ButtonOffer from "../buttons/ButtonOffer";
+import Tag from "../tag/Tag";
+
 export default function Card() {
   return (
     <article className=" max-w-xl border border-[var(--primary-color)] rounded-md shadow-lg shadow-slate-300 min-h-44 p-4 bg-[var(--secondary-background-color)] mb-4 max-md:max-w-96 ">
-            <header className="flex justify-between items-center mb-4">
-              <h3 className="text-[var(--primary-color)] max-md:text-lg">
-                Title-3{" "}
-              </h3>
-              <img src="./src/assets/images/signet.svg" alt="" />
-            </header>
-            <ul className="flex gap-1 relative mb-4">
-              <li className="w-14 bg-[var(--secondary-color)] text-[var(--primary-background-color)] text-center max-md:w-10 max-md:text-xs">
-                REACT
-              </li>
-              <li className="w-14 bg-[var(--secondary-color)] text-[var(--primary-background-color)] text-center max-md:w-10 max-md:text-xs">
-                REACT
-              </li>
-              <li className="w-14 bg-[var(--secondary-color)] text-[var(--primary-background-color)] text-center max-md:w-10 max-md:text-xs">
-                REACT
-              </li>
-              <li className="w-28 bg-[var(--secondary-color)] text-[var(--primary-background-color)] text-center absolute right-0 max-md:w-20 max-md:text-xs">
-                BORDEAUX
-              </li>
-            </ul>
-            <p className="text-content mb-4 max-md:text-sm">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
-              perspiciatis officia consequuntur expedita, sequi doloremque
-              dolor, aut quasi exercitationem doloribus dicta, facere ipsum
-              rerum ratione cumque. Aperiam debitis sit eaque.
-            </p>
-            <footer className="flex justify-center">
-              <button
-                className="w-40 h-8 bg-[var(--primary-color)] font-semibold text-[var(--primary-background-color)] rounded-lg cursor-pointer max-md:w-36 max-md:h-8 max-md:text-sm"
-                type="button"
-              >
-                LOREM IPSUM
-              </button>
-            </footer>
-          </article>
-  )
+      <header className="flex justify-between items-center mb-4">
+        <h3 className="text-[var(--primary-color)] max-md:text-lg">Title-3 </h3>
+        <img src="./src/assets/images/signet.svg" alt="" />
+      </header>
+      <ul className="flex gap-1 relative mb-4">
+        <Tag tag="REACT" />
+        <Tag tag="JAVA" />
+        <Tag tag="CSS" />
+      </ul>
+      <p className="text-content mb-4 max-md:text-sm">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam
+        perspiciatis officia consequuntur expedita, sequi doloremque dolor, aut
+        quasi exercitationem doloribus dicta, facere ipsum rerum ratione cumque.
+        Aperiam debitis sit eaque.
+      </p>
+      <footer className="flex justify-center">
+<ButtonOffer/>
+      </footer>
+    </article>
+  );
 }
