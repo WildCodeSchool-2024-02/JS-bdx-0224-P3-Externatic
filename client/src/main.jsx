bfbee19156378b3ad0b695175213443031433033
Enzo Michel <enzomichel399@gmail.com>
2024-06-13 12:21:04 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider />
   </React.StrictMode>
 );
