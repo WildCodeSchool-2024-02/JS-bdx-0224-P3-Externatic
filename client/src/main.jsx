f79b8d5e32c593a9b6461e4bc9b8c7b66e486ef4
Enzo Michel <enzomichel399@gmail.com>
2024-06-13 11:54:30 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,21 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./pages/Home";
 import App from "./App";
 import "../index.css";
-
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
