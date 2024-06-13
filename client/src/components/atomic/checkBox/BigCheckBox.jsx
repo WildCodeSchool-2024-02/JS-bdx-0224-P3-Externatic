0c32973b236f2e9735e755e80dbacd17554b4564
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-06-13 15:17:26 +0200

    transform components

--- client/src/components/atomic/checkBox/BigCheckBox.jsx
+++ client/src/components/atomic/checkBox/BigCheckBox.jsx
@@ -2,7 +2,6 @@ function BigCheckBox() {
   return (
     <input
       type="checkbox"
-      value=""
       className="w-5 h-5 accent-[var(--primary-color)] hover:accent-[var(--primary-color)] "
     />
   );
