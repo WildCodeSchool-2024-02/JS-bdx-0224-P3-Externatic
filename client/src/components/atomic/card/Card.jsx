7675264abddb147a13465519c515bf408292974e
AXEL JANIAK <axeljpro@gmail.com>
2024-06-13 14:45:26 +0200

    delete value

--- client/src/components/atomic/card/Card.jsx
+++ client/src/components/atomic/card/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
     <article className=" max-w-xl border border-[var(--primary-color)] rounded-md shadow-lg shadow-slate-300 min-h-44 p-4 bg-[var(--secondary-background-color)] mb-4 max-md:max-w-96 ">
       <header className="flex justify-between items-center mb-4">
         <h3 className="text-[var(--primary-color)] max-md:text-lg">Title-3 </h3>
-        <img src="./src/assets/images/signet.svg" alt="" />
+        <img src="./src/assets/images/signet.svg" alt=""/>
       </header>
       <ul className="flex gap-1 relative mb-4">
         <Tag tag="REACT" />
@@ -21,7 +21,7 @@ export default function Card() {
         Aperiam debitis sit eaque.
       </p>
       <footer className="flex justify-center">
-<ButtonOffer/>
+        <ButtonOffer />
       </footer>
     </article>
   );
--- client/src/components/atomic/checkBox/SmallCheckBox.jsx
+++ client/src/components/atomic/checkBox/SmallCheckBox.jsx
@@ -2,7 +2,6 @@ function SmallCheckBox() {
   return (
     <input
       type="checkbox"
-      value=""
       className=" accent-[var(--primary-color)] hover:accent-[var(--primary-color)]"
     />
   );
