d32d3d3ced823ad231fe72258f756b2d3163d756
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-06-17 14:36:05 +0200

    delete balise section in FormInputConsultant

--- client/src/components/atomic/inputCandidat/formCandidat/FormInputCandidat.jsx
+++ client/src/components/atomic/inputCandidat/formCandidat/FormInputCandidat.jsx
@@ -2,14 +2,17 @@ import PropTypes from "prop-types";
 
 export default function FormInputCandidat({ id, label, type }) {
   return (
-      <label className="text-[var(--primary-color)] font-medium max-md:text-sm max-md:font-normal w-72 flex flex-col mb-4 gap-4 max-sm:w-64" htmlFor={id}>
-        {label}
+    <label
+      className="text-[var(--primary-color)] font-medium max-md:text-sm max-md:font-normal w-72 flex flex-col mb-4 gap-4 max-sm:w-64"
+      htmlFor={id}
+    >
+      {label}
       <input
         className=" h-10 border-2 outline-[var(--primary-color)] rounded-s px-2"
         id={id}
         type={type}
-        />
-        </label>
+      />
+    </label>
   );
 }
 
--- client/src/components/atomic/inputConsultant/formConsultant/FormInputConsultant.jsx
+++ client/src/components/atomic/inputConsultant/formConsultant/FormInputConsultant.jsx
@@ -2,13 +2,11 @@ import PropTypes from "prop-types";
 
 export default function FormInputConsultant({ id, label, type }) {
   return (
-    <section className="w-[34rem] flex flex-col mb-4 gap-4 max-sm:w-64 max-md:w-96">
-      <label
-        className="text-[var(--primary-color)] font-medium max-md:text-sm max-md:font-normal"
-        htmlFor={id}
-      >
-        {label}
-      </label>
+    <label
+      className="w-[34rem] flex flex-col mb-4 gap-4 text-[var(--primary-color)] font-medium max-sm:w-64 max-md:w-96 max-md:text-sm max-md:font-normal "
+      htmlFor={id}
+    >
+      {label}
       {type === "textarea" ? (
         <textarea
           className="h-72 border-2 outline-[var(--primary-color)] rounded-s px-2 py-2"
@@ -21,7 +19,7 @@ export default function FormInputConsultant({ id, label, type }) {
           type={type}
         />
       )}
-    </section>
+    </label>
   );
 }
 FormInputConsultant.propTypes = {
