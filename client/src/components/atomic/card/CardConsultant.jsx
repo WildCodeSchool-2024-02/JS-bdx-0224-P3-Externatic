ccaa1a5997b70e17473b396a8f42b2139fd7d56f
AXEL JANIAK <axeljpro@gmail.com>
2024-06-18 11:03:57 +0200

    edit tag

--- client/src/components/atomic/card/CardConsultant.jsx
+++ client/src/components/atomic/card/CardConsultant.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import Button from "../buttons/Button";
 import Tag from "../tag/Tag";
-import TagStatus from "../tag/TagStatus";
 
 export default function CardConsultant({
   name = "Nom Prénom",
@@ -11,21 +10,17 @@ export default function CardConsultant({
     <article className="animate-fade-up animate-once animate-duration-700 animate-delay-200 animate-ease-in-out animate-alternate max-w-xl border border-[var(--primary-color)] rounded-md shadow-lg custom-shadow min-h-44 p-4 bg-[var(--secondary-background-color)] mb-4 max-md:max-w-96 ">
       <header className="flex justify-between items-center mb-4">
         <h3 className="text-[var(--primary-color)] max-md:text-lg">{name}</h3>
-        <TagStatus
-          text="DISPONIBLE"
-          textColor="text-[var(--validation-color-ok)]"
-          borderColor="border-[var(--validation-color-ok)]"
-        />
+        <Tag text="DISPONIBLE" apply="tagStatusGreen" />
       </header>
       <ul className="flex gap-1 relative mb-4">
         <li>
-          <Tag tag="REACT" />
+          <Tag text="REACT" apply="tag" />
         </li>
         <li>
-          <Tag tag="JAVA" />
+          <Tag text="JAVA" apply="tag" />
         </li>
         <li>
-          <Tag tag="CSS" />
+          <Tag text="CSS" apply="tag" />
         </li>
       </ul>
       <p className="text-content mb-4 max-md:text-sm">{textContent}</p>
