20cca8ec235cc078ae1d43e2b35ef35a8700b4a6
AXEL JANIAK <axeljpro@gmail.com>
2024-06-19 16:38:25 +0200

    edit responsive slider

--- client/src/components/atomic/slider/Slider.jsx
+++ client/src/components/atomic/slider/Slider.jsx
@@ -7,7 +7,7 @@ import "swiper/css/pagination";
 export default function Slider() {
   return (
       <Swiper
-        slidesPerView="6"
+        slidesPerView="2"
         spaceBetween={30}
         autoplay={{
           delay: 2000,
@@ -19,6 +19,20 @@ export default function Slider() {
         pagination={{
           clickable: true,
         }}
+        breakpoints={{
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 6,
+            spaceBetween: 30,
+          },
+        }}
         modules={[Pagination, Keyboard, Autoplay]}
         className=" mt-20 mb-20 pl-16 pr-16"
       >
@@ -26,105 +40,105 @@ export default function Slider() {
           <img
             src="./src/assets/images/company/Akeneo.png"
             alt=""
-            className=" w-52 pb-5"
+            className=" max-md:w-52 pb-5"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Decath.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Allovoisins.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/GIE.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Nickel.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Lucca.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Groupama.jpg"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Maincare.jpg"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/IAdvize.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Automotive.jpg"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Lengow.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Manitou.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/IKKS.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/Showroom .png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             src="./src/assets/images/company/mdm.png"
             alt=""
-            className="w-52"
+            className="max-md:w-52"
           />
         </SwiperSlide>
       </Swiper>
