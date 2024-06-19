2e568230383e477eb12aff2953ddb7e4f8f6a763
AXEL JANIAK <axeljpro@gmail.com>
2024-06-19 16:27:23 +0200

    add slider

--- client/src/components/atomic/slider/Slider.jsx
+++ client/src/components/atomic/slider/Slider.jsx
@@ -1,5 +1,132 @@
-function Slider() {
-  return <h1>slider</h1>;
-}
+/* eslint-disable import/no-unresolved */
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Keyboard } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
-export default Slider;
+export default function Slider() {
+  return (
+      <Swiper
+        slidesPerView="6"
+        spaceBetween={30}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Keyboard, Autoplay]}
+        className=" mt-20 mb-20 pl-16 pr-16"
+      >
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Akeneo.png"
+            alt=""
+            className=" w-52 pb-5"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Decath.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Allovoisins.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/GIE.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Nickel.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Lucca.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Groupama.jpg"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Maincare.jpg"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/IAdvize.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Automotive.jpg"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Lengow.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Manitou.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/IKKS.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/Showroom .png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./src/assets/images/company/mdm.png"
+            alt=""
+            className="w-52"
+          />
+        </SwiperSlide>
+      </Swiper>
+  );
+}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import Presentation from "../components/Presentation";
+import Slider from "../components/atomic/slider/Slider";
 
 function HomePage() {
   return (
+    <>
       <Presentation />
+      <Slider />
+    </>
   );
 }
 
