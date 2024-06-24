ae90c80c8b91bba817f907926653f9b5598aff86
Enzo Michel <enzomichel399@gmail.com>
2024-06-24 14:11:23 +0200

    fix

--- client/src/components/Presentation.jsx
+++ client/src/components/Presentation.jsx
@@ -30,7 +30,7 @@ function Presentation({handleChange}) {
         Notre cabinet de recrutement s’appuie sur des méthodes authentiques, où
         l’humain est tout simplement indispensable.
       </p>
-      <Button type="button" apply="medium" name="S'inscrire" className="" handleChange={handleChange} />
+      <Button type="button" apply="medium" name="S'inscrire" handleChange={handleChange} />
     </main>
   );
 };
