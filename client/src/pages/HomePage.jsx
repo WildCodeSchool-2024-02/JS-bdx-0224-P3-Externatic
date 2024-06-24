d11c5e31dbf4dca723c0848199594f61a6a0fee4
AXEL JANIAK <axeljpro@gmail.com>
2024-06-24 13:07:50 +0200

    add homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -10,14 +10,20 @@ function HomePage() {
   const [isVisible, setIsVisible] = useState(false);
 
   const handleChange = () => {
-
     setIsVisible(true);
-    setIsOpen(true);
+    if (isVisible) {
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 500);
+    }
+    setTimeout(() => {
+      setIsOpen(!isOpen);
+    }, 1);
   };
 
   const modalOpen = isOpen
-    ? "block bg-[var(--primary-color)] min-h-full min-w-56 absolute z-20 translate-x-0 top-0 delay-75 duration-500 text-[var(--text-content-size)] text-[var(--primary-background-color)]"
-    : "block duration-500 bg-[var(--primary-color)] min-h-screen min-w-56 absolute z-20 -translate-x-56 top-0 text-[var(--text-content-size)] text-[var(--primary-background-color)]";
+    ? "z-30"
+    : "";
 
   const modalVisible = isVisible ? "block" : "hidden";
 
