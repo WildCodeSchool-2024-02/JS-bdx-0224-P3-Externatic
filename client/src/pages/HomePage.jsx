ce1c737e6c27a5044dda149a2c7aeb7bc15fe349
Enzo Michel <enzomichel399@gmail.com>
2024-06-24 19:13:16 +0200

    fix linter

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import Presentation from "../components/Presentation";
 import Slider from "../components/Slider";
 import Values from "../components/Values";
-import ModalRegistration from "../components/ModalRegistration";
 
 function HomePage() {
 
