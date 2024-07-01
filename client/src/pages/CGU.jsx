1f2cb4dca2d1b75ede7d8380c2c2f16ababaaf31
JulienChevreauPro <julienchevreau33@gmail.com>
2024-07-01 10:33:31 +0200

    replace forgotten Society by Externatic

--- client/src/pages/CGU.jsx
+++ client/src/pages/CGU.jsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 function CGU() {
   return (
     <main className=" mt-10 mx-auto w-4/5">
-      <h1 className="my-8 text-[var(--secondary-color)]">Conditions Générales d'Utilisation</h1>
-      <h2 className="my-8 text-[var(--primary-color)]">1. Bienvenue sur Externatic</h2>
+      <h1 className="my-8 text-[var(--secondary-color)]">
+        Conditions Générales d'Utilisation
+      </h1>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        1. Bienvenue sur Externatic
+      </h2>
       <p className="my-8">
         Les présentes Conditions Générales d'Utilisation (les "CGU") régissent
         votre utilisation du site et des services fournis par Externatic. En
@@ -17,7 +21,9 @@ function CGU() {
         publier des offres d'emploi et aux candidats de rechercher des emplois
         dans le secteur des technologies de l'information (IT).
       </p>
-      <h2 className="my-8 text-[var(--primary-color)]">3. Inscription et Compte Utilisateur</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        3. Inscription et Compte Utilisateur
+      </h2>
       <p className="my-8">
         3.1. Pour utiliser certains services du site, vous devez créer un compte
         utilisateur en fournissant des informations exactes et complètes.
@@ -30,14 +36,18 @@ function CGU() {
         3.3. Vous vous engagez à informer immédiatement Externatic de toute
         utilisation non autorisée de votre compte.
       </p>
-      <h2 className="my-8 text-[var(--primary-color)]">4. Utilisation du site</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        4. Utilisation du site
+      </h2>
       <p className="my-8">
         4.1. Vous acceptez d'utiliser le site uniquement à des fins légales et
         conformément aux présentes CGU.
       </p>
       <ul className="my-8 text-lg">
         4.2. Il est interdit de :
-        <li className="ml-16">Publier des informations fausses ou trompeuses.</li>
+        <li className="ml-16">
+          Publier des informations fausses ou trompeuses.
+        </li>
         <li className="ml-16">
           Télécharger ou transmettre tout contenu illégal, nuisible, offensant
           ou diffamatoire.
@@ -47,10 +57,13 @@ function CGU() {
           tiers.
         </li>
         <li className="ml-16">
-          Tenter d'obtenir un accès non autorisé aux systèmes informatiques d'Externatic.
+          Tenter d'obtenir un accès non autorisé aux systèmes informatiques
+          d'Externatic.
         </li>
       </ul>
-      <h2 className="my-8 text-[var(--primary-color)]">5. Contenu Utilisateur</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        5. Contenu Utilisateur
+      </h2>
       <p className="my-8">
         5.1. En publiant du contenu sur le site, vous accordez à Externatic une
         licence mondiale, non exclusive, gratuite et transférable d'utiliser,
@@ -75,17 +88,28 @@ function CGU() {
         indirects résultant de l'utilisation du site ou de l'incapacité à
         utiliser le site.
       </p>
-      <h2 className="my-8 text-[var(--primary-color)]">7. Protection des Données</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        7. Protection des Données
+      </h2>
       <p className="my-8">
         7.1. La collecte et le traitement des données personnelles sont régis
-        par notre <Link to="/protectionDataPolicy" className="text-[var(--primary-color)]">Politique de Confidentialité</Link>.
+        par notre{" "}
+        <Link
+          to="/protectionDataPolicy"
+          className="text-[var(--primary-color)]"
+        >
+          Politique de Confidentialité
+        </Link>
+        .
       </p>
       <p className="my-8">
         7.2. En utilisant Externatic, vous consentez à la collecte et à
         l'utilisation de vos données personnelles conformément à cette
         politique.
       </p>
-      <h2 className="my-8 text-[var(--primary-color)]">8. Modification des CGU</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        8. Modification des CGU
+      </h2>
       <p>
         {" "}
         Externatic se réserve le droit de modifier les présentes CGU à tout
@@ -103,7 +127,10 @@ function CGU() {
         9.2. Vous pouvez fermer votre compte à tout moment en contactant le
         service client d'Externatic.
       </p>
-      <h2 className="my-8 text-[var(--primary-color)]"> 10. Droit Applicable et Juridiction</h2>
+      <h2 className="my-8 text-[var(--primary-color)]">
+        {" "}
+        10. Droit Applicable et Juridiction
+      </h2>
       <p className="my-8">
         Les présentes CGU sont régies par le droit français. Tout litige relatif
         à l'interprétation ou à l'exécution des présentes CGU sera soumis à la
@@ -111,8 +138,9 @@ function CGU() {
       </p>
       <h2 className="my-8 text-[var(--primary-color)]"> 11. Contact</h2>
       <p className="my-8">
-        Pour toute question concernant les présentes CGU, veuillez contacter la
-        Société à l'adresse suivante : Service client, Externatic, 33000 Bordeaux.
+        Pour toute question concernant les présentes CGU, veuillez contacter
+        Externatic à l'adresse suivante : Service client, Externatic, 33000
+        Bordeaux.
       </p>
     </main>
   );
