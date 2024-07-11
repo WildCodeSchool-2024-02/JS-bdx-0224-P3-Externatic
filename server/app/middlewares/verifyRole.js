baf3e59f9e77f4cafef92e455c793d96802dd7ee
Enzo Michel <enzomichel399@gmail.com>
2024-07-11 13:10:23 +0200

    fix return verifyToken

--- server/app/middlewares/verifyRole.js
+++ server/app/middlewares/verifyRole.js
@@ -1,9 +1,13 @@
 const verifyRole = (req, res, next) => {
-  if (req.auth.role !== "consultant") {
-    return res.sendStatus(403);
+  try {
+    const isConsultant = req.auth.role === "consultant";
+    if (!isConsultant) {
+      return res.sendStatus(403);
+    }
+    return next();
+  } catch (error) {
+    return next(error); // Passe l'erreur au middleware de gestion des erreurs
   }
-  next();
-  return null;
 };
 
 module.exports = { verifyRole };
