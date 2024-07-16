2cfc6a9e35dfc00b1a42eab55a951b86f1fc67e9
AXEL JANIAK <axeljpro@gmail.com>
2024-07-16 14:32:09 +0200

    finish slider offerFavoris

--- client/src/pages/DashboardCandidate.jsx
+++ client/src/pages/DashboardCandidate.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable import/no-unresolved */
 import { useLoaderData } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Keyboard, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import FileUpload from "../components/atomic/FileUpload";
 import Tag from "../components/atomic/tag/Tag";
 import Button from "../components/atomic/buttons/Button";
 import CardOfferForCandidate from "../components/atomic/card/CardOfferForCandidate";
+
+import "../../index.css";
 
 function DashboardCandidate() {
   const data = useLoaderData();
@@ -16,8 +24,9 @@ function DashboardCandidate() {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         });
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        const result = await response.json()
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`);
+        const result = await response.json();
         setFavorites(result);
       } catch (err) {
         console.error("Error fetching favorites", err);
@@ -41,9 +50,13 @@ function DashboardCandidate() {
             <li>Tel: {data.phone || "Non renseigné"}</li>
             <li>Localisation: {data.name || "Non renseigné"}</li>
           </ul>
-          <img src={data.picture || 'https://picsum.photos/200'} alt="" className="md:w-2/12 w-1/2 rounded-full" />
+          <img
+            src={data.picture || "https://picsum.photos/200"}
+            alt=""
+            className="md:w-2/12 w-1/2 rounded-full"
+          />
         </header>
-        <FileUpload/>
+        <FileUpload />
         <h2 className="text-[var(--primary-color)] pb-3">Mes compétences</h2>
         <ul className="flex flex-wrap gap-5">
           {data.technos && data.technos.length > 0 ? (
@@ -58,14 +71,50 @@ function DashboardCandidate() {
         </ul>
         <h2 className="text-[var(--primary-color)] pb-3 pt-10">Mes favoris</h2>
         <ul className="flex flex-wrap gap-5">
-          {favorites.length > 0 ? (
-            favorites.map((offer) => (
-              <li key={offer.id}>
-                <CardOfferForCandidate offer={offer} />
-              </li>
-            ))
+          {favorites.length === 1 ? (
+            <li key={favorites[0].id}>
+              <CardOfferForCandidate offer={favorites[0]} />
+            </li>
           ) : (
-            <li className="list-none">Pas de favoris sélectionnés</li>
+            <Swiper
+              slidesPerView="1"
+              spaceBetween={10}
+              keyboard={{
+                enabled: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                1024: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation
+              modules={[Keyboard, Pagination, Navigation]}
+              className="mySwiper"
+            >
+              {favorites.length > 0 ? (
+                favorites.map((offer) => (
+                  <SwiperSlide key={offer.id}>
+                    <li key={offer.id}>
+                      <CardOfferForCandidate offer={offer} />
+                    </li>
+                  </SwiperSlide>
+                ))
+              ) : (
+                <li className="list-none">Pas de favoris sélectionnés</li>
+              )}
+            </Swiper>
           )}
         </ul>
       </article>
